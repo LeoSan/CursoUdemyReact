4e27ae033482d0521f8b8ba5a117592bda316a1f
LeoSan <cuenca623@gmail.com>
2023-02-28 22:36:26 -0600

    ajuste

--- ReactAvanzado/Proyectos/try-app/src/components/routes/routes.ts
+++ ReactAvanzado/Proyectos/try-app/src/components/routes/routes.ts
@@ -14,8 +14,6 @@ interface Route {
 
 }
 
-
-
 const Lazy1 = lazy(()=>import(/* webpackChunkName: "LazyPage1"*/'../../lazyload/pages/LazyPageone'));
 const Lazy2 = lazy(()=>import(/* webpackChunkName: "LazyPage2"*/'../../lazyload/pages/LazyPagetwo'));
 const Lazy3 = lazy(()=>import(/* webpackChunkName: "LazyPage3"*/'../../lazyload/pages/LazyPagetree'));
